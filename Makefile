868d94cca481da386db174b6030a2fb91d4f60ac
Annwn <annwncode@gmail.com>
2023-08-28 18:31:58 +0100

    Add sdiss (Sinistra Disassembler)
    This has also necessitated a rewriting of the Makefile.

--- Makefile
+++ Makefile
@@ -1,42 +1,75 @@
-CC=gcc
-CFLAGS=-g -Wall -MMD -MP
-LDFLAGS=
-LIBS=
-YACC=bison
-LEX=flex
-DEBUG=-DDEBUG=1
+CC = gcc
+CFLAGS = -g -Wall -MMD -MP
+LDFLAGS =
+LIBS =
+YACC = bison
+LEX = flex
+DEBUG = -DDEBUG=1
 
 SRC_DIR := src
 OBJ_DIR := obj
+LIB_DIR := lib
 
-# Collect source files (excluding scomp.c) and generate object file paths
-SOURCES := $(filter-out $(SRC_DIR)/scomp.c, $(wildcard $(SRC_DIR)/*.c))
-OBJECTS := $(patsubst $(SRC_DIR)/%.c,$(OBJ_DIR)/%.o,$(SOURCES))
+# Dependency files
 DEPS := $(OBJECTS:.o=.d)
+
+# Library of shared functions
+LIB := $(LIB_DIR)/libsinshared.a
+LIB_OBJECTS := $(OBJ_DIR)/log.o $(OBJ_DIR)/memory.o
+
+# Source files for scomp
+SCOMP_SOURCES := $(SRC_DIR)/scomp.c
+SCOMP_OBJECTS := $(SCOMP_SOURCES:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+
+# Parser files for scomp
+PARSER_SOURCES := $(SRC_DIR)/parser.y
+PARSER_GENERATED := $(SRC_DIR)/parser.c $(SRC_DIR)/parser.h
+
+# Lexer files for scomp
+LEXER_SOURCES := $(SRC_DIR)/lexer.l
+LEXER_GENERATED := $(SRC_DIR)/lexer.c
+
+# Source files for sdiss
+SDISS_SOURCES := $(SRC_DIR)/sdiss.c
+SDISS_OBJECTS := $(SDISS_SOURCES:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+
+# Source files for sin
+SIN_SOURCES := $(SRC_DIR)/sin.c $(SRC_DIR)/interpret.c \
+               $(SRC_DIR)/item.c $(SRC_DIR)/stack.c $(SRC_DIR)/value.c
+SIN_OBJECTS := $(SIN_SOURCES:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
 
 $(OBJ_DIR)/%.o : $(SRC_DIR)/%.c
 	@mkdir -p $(@D)
 	$(CC) -c $(CFLAGS) $(DEBUG) $< -o $@
 
-.PHONY: all clean
+.PHONY: all clean lib
 
-all: sin scomp
+all: $(LIB) scomp sdiss sin
+
+lib: $(LIB)
+
+$(LIB): $(LIB_OBJECTS)
+	@mkdir -p $(LIB_DIR)
+	ar rcs $@ $^
+
+scomp: $(PARSER_GENERATED) $(LEXER_GENERATED) $(SCOMP_OBJECTS) $(LIB)
+	$(CC) -o $@ $^ $(LDFLAGS) $(LIBS)
+
+sdiss: $(SDISS_OBJECTS) $(LIB)
+	$(CC) -o $@ $^ $(LDFLAGS) $(LIBS)
+
+sin: $(SIN_OBJECTS) $(LIB)
+	$(CC) -o $@ $^ $(LDFLAGS) $(LIBS)
+
+$(PARSER_GENERATED): $(PARSER_SOURCES)
+	$(YACC) -o $(SRC_DIR)/parser.c --defines=$(SRC_DIR)/parser.h $<
+
+$(LEXER_GENERATED): $(LEXER_SOURCES) $(PARSER_GENERATED)
+	$(LEX) -o $(SRC_DIR)/lexer.c $<
+
+# Include dependency files
+-include $(DEPS)
 
 clean:
-	rm -rf $(OBJ_DIR)/*.o $(OBJ_DIR)/*.d sin scomp $(SRC_DIR)/parser.c \
-         $(SRC_DIR)/parser.h $(SRC_DIR)/lexer.c
+	rm -rf $(OBJ_DIR)/*.o $(OBJ_DIR)/*.d $(LIB) $(LIB_DIR)
 
-sin: $(OBJECTS)
-	$(CC) -o $@ $^ $(LDFLAGS) $(LIBS)
-
-scomp: $(OBJ_DIR)/parser.o $(OBJ_DIR)/lexer.o $(OBJ_DIR)/memory.o $(OBJ_DIR)/log.o $(OBJ_DIR)/scomp.o
-	$(CC) -o $@ $^ $(LDFLAGS) $(LIBS)
- 
-$(SRC_DIR)/parser.c $(SRC_DIR)/parser.h: $(SRC_DIR)/parser.y
-	$(YACC) -o $(SRC_DIR)/parser.c --defines=$(SRC_DIR)/parser.h \
-          $(SRC_DIR)/parser.y
-  
-$(SRC_DIR)/lexer.c: $(SRC_DIR)/lexer.l $(SRC_DIR)/parser.h
-	$(LEX) -o $(SRC_DIR)/lexer.c $(SRC_DIR)/lexer.l
-
--include $(DEPS)
---
+++ src/sdiss.c
@@ -0,0 +1,196 @@
+// sdiss - Sinistra bytecode disassembler
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <getopt.h>
+
+#include "memory.h"
+#include "log.h"
+#include "value.h"
+#include "item.h"
+#include "stack.h"
+
+void usage() {
+  logmsg("Sinistra disassembler.\nSyntax: sdiss <options>\n");
+  logmsg("Options:\n");
+  logmsg(" -h, --help\t\tThis message.\n");
+  logmsg(" -o, --object <file>\tObject code to disassemble.\n");
+}
+
+int main(int argc, char **argv) {
+  FILE *in;
+  int filesize = 0;
+  uint8_t *bytecode = NULL, *opcodeptr;
+
+  if (argc < 2) {
+    usage();
+    exit(EXIT_FAILURE);
+  }
+
+  // Are there any interesting options?
+  int opt;
+  const struct option options[] =
+  {
+    {"help", no_argument, 0, 'h'},
+    {"object", required_argument, 0, 'o'},
+    {NULL, 0, 0, '\0'}
+  };
+  while ((opt = getopt_long(argc, argv, "ho:", options, NULL)) != -1) {
+    switch(opt) {
+      case 'h':
+        usage();
+        exit(EXIT_SUCCESS);
+        break;
+      case 'o':
+        // Mandatory: Name of the object code file.
+        in = fopen(optarg, "r");
+        if (!in) {
+          logerr("Unable to open input file: %s\n", optarg);
+          exit(EXIT_FAILURE);
+        }
+        fseek(in, 0, SEEK_END);
+        filesize = ftell(in);
+        fseek(in, 0, SEEK_SET);
+        bytecode = GROW_ARRAY(unsigned char, bytecode, 0, filesize);
+        fread(bytecode, filesize, sizeof(char), in);
+        fclose(in);
+        logmsg("Bytecode loaded: %d bytes.\n", filesize);
+        break;
+      default:
+        usage();
+        return EXIT_FAILURE;
+    }
+  }
+
+  // Just check to see if we have been given some bytecode.
+  if (!bytecode) {
+    logerr("No bytecode to process!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // We have some bytecode.  Step through it and output some helpful
+  // disassembly.  Hopefully helpful, anyway.
+  logmsg("Beginning disassembly...\n");
+  opcodeptr = bytecode;
+
+  // First, do we have any locals?
+  uint8_t locals = *opcodeptr;
+  opcodeptr++;
+  if (locals > 0) {
+    logmsg("Local variables: %d\n", locals);
+  } else {
+    logmsg("No local variables.\n");
+  }
+
+  // Locals processed, so now step through the bytecode until the HALT
+  // instruction is found.  This is just one big switch.
+  while (*opcodeptr != 'h') {
+    int16_t offset;
+    int64_t ival;
+    switch (*opcodeptr) {
+      case 'a':
+        logmsg("ADD\n");
+        opcodeptr++;
+        break;
+      case 'c':
+        opcodeptr++;
+        logmsg("SAVE LOCAL %d\n", *opcodeptr);
+        opcodeptr++;
+        break;
+      case 'd':
+        logmsg("DIVIDE\n");
+        opcodeptr++;
+        break;
+      case 'e':
+        opcodeptr++;
+        logmsg("RETRIEVE LOCAL %d\n", *opcodeptr);
+        opcodeptr++;
+        break;
+      case 'f':
+        opcodeptr++;
+        logmsg("INCREMENT LOCAL %d\n", *opcodeptr);
+        opcodeptr++;
+        break;
+      case 'g':
+        opcodeptr++;
+        logmsg("DECREMENT LOCAL %d\n", *opcodeptr);
+        opcodeptr++;
+        break;
+      case 'j':
+        opcodeptr++;
+        offset = *(int16_t*)opcodeptr;
+        opcodeptr += 2;
+        logmsg("JUMP %d\n", offset);
+        break;
+      case 'k':
+        opcodeptr++;
+        offset = *(int16_t*)opcodeptr;
+        opcodeptr += 2;
+        logmsg("JUMP IF FALSE %d\n", offset);
+        break;
+      case 'l':
+        opcodeptr++;
+        offset = *(int16_t*)opcodeptr; // Length of string
+        opcodeptr += 2;
+        logmsg("STRINGLIT: ");
+        for (uint16_t s = 0; s < offset; s++) {
+          logmsg("%c", *opcodeptr);
+          opcodeptr++;
+        }
+        logmsg("\n");
+        break;
+      case 'm':
+        logmsg("MULTIPLY\n");
+        opcodeptr++;
+        break;
+      case 'n':
+        logmsg("NEGATE\n");
+        opcodeptr++;
+        break;
+      case 'o':
+        logmsg("BOOL EQ\n");
+        opcodeptr++;
+        break;
+      case 'p':
+        opcodeptr++;
+        ival = *(int64_t*)opcodeptr;
+        logmsg("INTEGER %ld\n", ival);
+        opcodeptr += 8;
+        break;
+      case 'q':
+        logmsg("BOOL NOTEQ\n");
+        opcodeptr++;
+        break;
+      case 'r':
+        logmsg("BOOL LT\n");
+        opcodeptr++;
+        break;
+      case 's':
+        logmsg("SUBTRACT\n");
+        opcodeptr++;
+        break;
+      case 't':
+        logmsg("BOOL GT\n");
+        opcodeptr++;
+        break;
+      case 'u':
+        logmsg("BOOL LTEQ\n");
+        opcodeptr++;
+        break;
+      case 'v':
+        logmsg("BOOL GTEQ\n");
+        opcodeptr++;
+        break;
+      default:
+        logerr("Undefined opcode: %c (%d)\n", *opcodeptr, *opcodeptr);
+        opcodeptr++;
+    }
+  }
+  logmsg("HALT\n");
+
+  // Clean up
+  logmsg("Shutting down.\n");
+  FREE_ARRAY(unsigned char, bytecode, filesize);
+  exit(EXIT_SUCCESS);
+}
+
