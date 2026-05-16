0aa7602f0d1655e14fd0b4770a3b8c2e567f1b4f
archaro <archaro@users.noreply.github.com>
2026-05-16 08:12:11 +0100

    Refactor local index resolution in lowering

--- tests/test_ir_validate.c
+++ tests/test_ir_validate.c
@@ -4,6 +4,8 @@
 
 #include "error.h"
 #include "ir.h"
+#include "lower.h"
+#include "semant.h"
 #include "test_assert.h"
 #include "test_helpers.h"
 
@@ -89,6 +91,38 @@ static void test_ir_validate_libcall_negative_function_index_rejected(void) {
   ir_destroy_unit(unit);
 }
 
+static void assert_lower_local_error(AS_NODE *root, const char *expected_name) {
+  IR_Unit *ir = NULL;
+  char *errdetail = NULL;
+  int8_t rc = lower_ast_to_ir(root, NULL, &ir, &errdetail);
+  ASSERT_EQ_INT(ERR_COMP_LOCALBEFOREDEF, rc);
+  ASSERT_TRUE(ir == NULL);
+  ASSERT_NOT_NULL(errdetail);
+  ASSERT_TRUE(strstr(errdetail, expected_name) != NULL);
+  free(errdetail);
+}
+
+static void test_lower_local_resolution_errors_consistent(void) {
+  AS_NODE *expr_stmt = t_node(N_EXPRSTMT, t_local("x"), NULL);
+  AS_NODE *asslocal = t_node(N_ASSLOCAL, t_local("x"), t_int(1));
+  AS_NODE *inc = t_node(N_INC, t_local("x"), NULL);
+  AS_NODE *dec = t_node(N_DEC, t_local("x"), NULL);
+  AS_NODE *null_named_local = as_new_node(N_VALUE, as_new_value(V_LOCAL, 0, NULL), NULL);
+  AS_NODE *null_expr_stmt = t_node(N_EXPRSTMT, null_named_local, NULL);
+
+  assert_lower_local_error(expr_stmt, "x");
+  assert_lower_local_error(asslocal, "x");
+  assert_lower_local_error(inc, "x");
+  assert_lower_local_error(dec, "x");
+  assert_lower_local_error(null_expr_stmt, "<null>");
+
+  as_delete(expr_stmt);
+  as_delete(asslocal);
+  as_delete(inc);
+  as_delete(dec);
+  as_delete(null_expr_stmt);
+}
+
 void test_ir_validate(void) {
   test_ir_validate_ok_case();
   test_ir_validate_unbound_label_rejected();
@@ -96,4 +130,5 @@ void test_ir_validate(void) {
   test_ir_validate_local_index_out_of_range_rejected();
   test_ir_validate_negative_arity_rejected();
   test_ir_validate_libcall_negative_function_index_rejected();
+  test_lower_local_resolution_errors_consistent();
 }
