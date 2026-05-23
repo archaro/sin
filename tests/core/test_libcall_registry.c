c90fef97515861aa99c10a54d5b6b7cfa69a8b70
archaro <archaro@users.noreply.github.com>
2026-05-23 12:27:17 +0100

    Refactor libcall invalid-arg handling and add regression tests

--- tests/core/test_libcall_registry.c
+++ tests/core/test_libcall_registry.c
@@ -10,7 +10,33 @@
 #include "vm.h"
 #include "memory.h"
 
+#include "network.h"
+
+uint8_t *lc_task_newgametask(uint8_t *nextop, ITEM_t *item);
+uint8_t *lc_task_killtask(uint8_t *nextop, ITEM_t *item);
+uint8_t *lc_net_write(uint8_t *nextop, ITEM_t *item);
+
+extern LINE_t *line;
 extern CONFIG_t config;
+
+static void setup_libcall_runtime(void) {
+  memset(&config, 0, sizeof(config));
+  init_errmsg();
+  config.itemroot = make_root_item("root");
+  ASSERT_NOT_NULL(config.itemroot);
+  config.vm = make_vm();
+  ASSERT_NOT_NULL(config.vm);
+}
+
+static void teardown_libcall_runtime(void) {
+  if (line) {
+    free(line);
+    line = NULL;
+  }
+  destroy_vm(config.vm);
+  destroy_item(config.itemroot);
+}
+
 
 static uint8_t *test_noop_libcall(uint8_t *nextop, ITEM_t *item) {
   (void)item;
@@ -115,4 +141,46 @@ void test_libcall_registry_self_check_invalid_entries(void) {
 
   const LIBCALL_t gap_lib[] = {{"sys","a",1,0,0,test_noop_libcall},{"net","b",3,0,0,test_noop_libcall},{NULL,NULL,-1,-1,0,NULL}};
   ASSERT_TRUE(!libcall_registry_self_check(gap_lib, false));
+}
+
+void test_libcall_invalid_arg_branches_return_contracts(void) {
+  setup_libcall_runtime();
+
+  VALUE_t bad_name = {VALUE_int, {.i = 7}};
+  VALUE_t start = {VALUE_int, {.i = 1}};
+  VALUE_t repeat = {VALUE_int, {.i = 1}};
+  push_stack(config.vm->stack, bad_name);
+  push_stack(config.vm->stack, start);
+  push_stack(config.vm->stack, repeat);
+  (void)lc_task_newgametask(NULL, config.itemroot);
+  VALUE_t ret = pop_stack(config.vm->stack);
+  ASSERT_EQ_INT(VALUE_nil, ret.type);
+  ITEM_t *err = find_item(config.itemroot, "error");
+  ASSERT_NOT_NULL(err);
+  ASSERT_EQ_INT(ERR_RUNTIME_INVALIDARGS, err->value.i);
+
+  VALUE_t bad_taskid = {VALUE_str, {.s = strdup("x")}};
+  push_stack(config.vm->stack, bad_taskid);
+  (void)lc_task_killtask(NULL, config.itemroot);
+  ret = pop_stack(config.vm->stack);
+  ASSERT_EQ_INT(VALUE_nil, ret.type);
+  err = find_item(config.itemroot, "error");
+  ASSERT_NOT_NULL(err);
+  ASSERT_EQ_INT(ERR_RUNTIME_INVALIDARGS, err->value.i);
+
+  config.maxconns = 1;
+  line = calloc((size_t)config.maxconns, sizeof(LINE_t));
+  ASSERT_NOT_NULL(line);
+  VALUE_t bad_line = {VALUE_int, {.i = -1}};
+  VALUE_t out = {VALUE_str, {.s = strdup("hello")}};
+  push_stack(config.vm->stack, bad_line);
+  push_stack(config.vm->stack, out);
+  (void)lc_net_write(NULL, config.itemroot);
+  ret = pop_stack(config.vm->stack);
+  ASSERT_EQ_INT(VALUE_nil, ret.type);
+  err = find_item(config.itemroot, "error");
+  ASSERT_NOT_NULL(err);
+  ASSERT_EQ_INT(ERR_RUNTIME_INVALIDARGS, err->value.i);
+
+  teardown_libcall_runtime();
 }
