000b2604ad4aaca4970e3cc6065156a0cc901ce0
Annwn <annwncode@gmail.com>
2026-09-06 13:40:56 +0100

    test register time year boundary case

--- tests/rewrite/group6_adapter_libcall_time.c
+++ tests/rewrite/group6_adapter_libcall_time.c
@@ -9,6 +9,7 @@ void test_time_year_unrepresentable_timestamp_publishes_error(void);
 void test_time_year_source_integration_and_arity(void);
 void test_time_calendar_source_integration_and_arity(void);
 void test_time_formatted_utc_boundaries_and_negative_flooring(void);
+void test_time_formatted_year_boundaries(void);
 void test_time_fulldate_ordinal_suffixes_and_month_names(void);
 void test_time_formatted_rejects_invalid_types_and_publishes_details(void);
 void test_time_formatted_unrepresentable_timestamp_publishes_error(void);
@@ -25,6 +26,7 @@ static const TF_TestDescriptor tests[] = {
     {"rewrite.runtime.test_time_year_source_integration_and_arity", test_time_year_source_integration_and_arity, "exclusive", 30000, "api.libcall.time,language.token.tlibname,libcall.time.year"},
     {"rewrite.runtime.test_time_calendar_source_integration_and_arity", test_time_calendar_source_integration_and_arity, "exclusive", 30000, "api.libcall.time,language.token.tlibname,libcall.time.month,libcall.time.day,libcall.time.hour,libcall.time.minute,libcall.time.second"},
     {"rewrite.runtime.test_time_formatted_utc_boundaries_and_negative_flooring", test_time_formatted_utc_boundaries_and_negative_flooring, "exclusive", 30000, "api.libcall.time,libcall.time.timestamp,libcall.time.time,libcall.time.date,libcall.time.fulldate"},
+    {"rewrite.runtime.test_time_formatted_year_boundaries", test_time_formatted_year_boundaries, "exclusive", 30000, "api.libcall.time,libcall.time.timestamp,libcall.time.time,libcall.time.date,libcall.time.fulldate"},
     {"rewrite.runtime.test_time_fulldate_ordinal_suffixes_and_month_names", test_time_fulldate_ordinal_suffixes_and_month_names, "exclusive", 30000, "api.libcall.time,libcall.time.fulldate"},
     {"rewrite.runtime.test_time_formatted_rejects_invalid_types_and_publishes_details", test_time_formatted_rejects_invalid_types_and_publishes_details, "exclusive", 30000, "api.common.errors,api.libcall.time,libcall.time.timestamp,libcall.time.time,libcall.time.date,libcall.time.fulldate"},
     {"rewrite.runtime.test_time_formatted_unrepresentable_timestamp_publishes_error", test_time_formatted_unrepresentable_timestamp_publishes_error, "exclusive", 30000, "api.common.errors,api.libcall.time,libcall.time.timestamp"},
